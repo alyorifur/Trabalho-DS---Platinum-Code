0f494e49afd28965380c43ee356e00528dfa0e9d
alyf <168209059+alyorifur@users.noreply.github.com>
2025-04-15 08:13:02 -0300

    Add files via upload

---
+++ platinum_code/Form_sobre.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace platinum_code
+{
+    partial class Form_sobre
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_sobre));
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            richTextBox1 = new RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.ErrorImage = (Image)resources.GetObject("pictureBox1.ErrorImage");
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.InitialImage = (Image)resources.GetObject("pictureBox1.InitialImage");
+            pictureBox1.Location = new Point(441, 200);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(45, 47);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.Snow;
+            label1.Location = new Point(86, 209);
+            label1.Name = "label1";
+            label1.Size = new Size(349, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Desenvolvido pela empresa Platinum code";
+            label1.Click += label1_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.BackColor = Color.FromArgb(64, 64, 64);
+            richTextBox1.ForeColor = Color.Snow;
+            richTextBox1.Location = new Point(15, 41);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(545, 88);
+            richTextBox1.TabIndex = 3;
+            richTextBox1.Text = "O nosso sistema Maximus, feito para o mercado atacadinho, \nfoi feito para gerenciar e cadastrar a compra e venda de produtos, \naumentando a organização e o controle do mercado.";
+            richTextBox1.TextChanged += richTextBox1_TextChanged;
+            // 
+            // Form_sobre
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(64, 64, 64);
+            ClientSize = new Size(572, 286);
+            Controls.Add(richTextBox1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Name = "Form_sobre";
+            Text = "Sobre";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private RichTextBox richTextBox1;
+    }
+}
